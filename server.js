89dfbb13b492e7b998695391c6895bcf5e97b328
Яков Якубов <a777@MacBook-Pro-2.local>
2024-09-18 09:40:21 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { Client } = require("pg");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// Настройка подключения к базе данных
+const client = new Client({
+  user: "KIS_ASCLEPIUS_SYNC",
+  host: "m11-pgkisc-01", // IP-адрес сервера базы данных
+  database: "kis",
+  password: "6937D366",
+  port: 5432,
+});
+
+client.connect();
+
+// Маршрут для получения данных (с фильтрацией по датам)
+app.get("/data", async (req, res) => {
+  try {
+    const { startDate, endDate } = req.query;
+
+    // Если startDate и endDate не указаны, установим диапазон по умолчанию
+    const start = startDate || '2024-09-01';
+    const end = endDate || '2024-09-16';
+
+    console.log(`Получены даты: ${start} - ${end}`);
+
+    const query = `
+      SELECT 
+      INITCAP(CONCAT_WS(' ', md.surname, md.name, md.patron)) AS pat_fio,
+      na.name AS naz_name,
+      na.create_dt AS create_dt,
+      INITCAP(CONCAT_WS(' ', p_emp_create.surname, p_emp_create.name, p_emp_create.patron)) AS created_by,
+      na.sign_dt AS execute_dt,
+      INITCAP(CONCAT_WS(' ', p_emp_execute.surname, p_emp_execute.name, p_emp_execute.patron)) AS executed_by
+      FROM mm.naz na
+      INNER JOIN mm.naz_type nt ON nt.id = na.naz_type_id
+      INNER JOIN mm.naz_type_dir ntd ON ntd.id = nt.naz_type_dir_id
+      INNER JOIN (
+        SELECT path FROM mm.naz_type_dir nd WHERE 'GROUP_CONS' = ANY(tags)
+      ) pa ON pa.path @> ntd.path
+      JOIN mm.mdoc md ON md.id = na.mdoc_id
+      JOIN mm.hospdoc hd ON hd.mdoc_id = na.mdoc_id
+      JOIN mm.emp e_create ON e_create.id = na.creator_emp_id
+      JOIN mm.people p_emp_create ON p_emp_create.id = e_create.people_id
+      JOIN mm.emp e_execute ON e_execute.id = na.creator_emp_id
+      JOIN mm.people p_emp_execute ON p_emp_execute.id = e_execute.people_id
+      WHERE na.create_dt::DATE BETWEEN $1 AND $2
+    `;
+    
+    const result = await client.query(query, [start, end]);
+
+    res.json(result.rows); // Отправляем данные в формате JSON
+  } catch (err) {
+    console.error("Ошибка при выполнении запроса:", err);
+    res.status(500).json({ error: "Ошибка при выполнении запроса" });
+  }
+});
+
+// Обслуживание статических файлов (HTML, JS, CSS)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
